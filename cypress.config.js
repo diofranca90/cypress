21b81e685ce964cec4c86d065d7bb424b01e2bc7
Diorgenes França <diorgenesfranca@gmail.com>
2024-02-07 00:41:08 -0300

    configuração do "mochawesome" para relatórios

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,15 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      { "reporter"; "mochawesome",
+        "reporterOptions"; 
+        { "reportDir"; "cypress/report/mochawesome-report",
+          "overwrite"; true,
+          "html"; true,
+          "json"; true,
+          "timestamp"; "mmddyyyy_HHMMss"
+        }
+      }        
     },
   },
 });
